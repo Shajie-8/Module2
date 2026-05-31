e260879e4c2bbfd8dd36ebce6ff211277f99967c
Shajie-8 <shajie.macabalang@gmail.com>
2026-05-31 15:39:02 +0800

    Added private double semiMajorAxis and MinorAxis

--- src/Ellipse.java
+++ src/Ellipse.java
@@ -1,2 +1,8 @@
-public class Ellipse {
+// UNDERSTAND: This class models a geometric ellipse
+class Ellipse {
+    // UNDERSTAND: Private fields restrict direct modification from outside the class
+    // DECISION: Both stored as double to support fractional measurements
+    private double semiMajorAxis;
+    private double semiMinorAxis;
+
 }

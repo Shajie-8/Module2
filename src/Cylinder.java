39fde7a538c891c9359348b5621e056662e4702a
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 21:11:22 +0800

    Added private double radius and height, and constructor

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,2 +1,12 @@
 public class Cylinder {
+
+    // UNDERSTAND: Stores the radius and height
+    private double radius;
+    private double height;
+
+    // UNDERSTAND: Default constructor
+    Cylinder() {
+        radius = 1.0;
+        height = 1.0;
+    }
 }

9ddbb93f6a74cf73c29dc3497091c4d13393496f
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 20:44:50 +0800

    Added private double length, width, and height and also constructor

--- src/Cuboid.java
+++ src/Cuboid.java
@@ -1,2 +1,14 @@
 public class Cuboid {
+
+    // UNDERSTAND: Stores the dimensions of the cuboid
+    private double length;
+    private double width;
+    private double height;
+
+    // UNDERSTAND: Default values
+    Cuboid() {
+        length = 1.0;
+        width = 1.0;
+        height = 1.0;
+    }
 }

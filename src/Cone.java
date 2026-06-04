cc471373c404cb114e911c76e5b2a1cf1133c1c8
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 21:06:02 +0800

    Added cone dimensions and getSlantHeight

--- src/Cone.java
+++ src/Cone.java
@@ -9,4 +9,15 @@ public class Cone {
         radius = 1.0;
         height = 1.0;
     }
+
+    // UNDERSTAND: Creates a cone with given dimensions
+    Cone(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    // UNDERSTAND: Finds the slant height using Pythagorean Theorem
+    double getSlantHeight() {
+        return Math.sqrt(radius * radius + height * height);
+    }
 }

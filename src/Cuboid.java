8ba8d01650071c5ab17cf404944d4171f5b60011
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 20:50:55 +0800

    Added calculateSurfaceArea and calculateVolume

--- src/Cuboid.java
+++ src/Cuboid.java
@@ -11,4 +11,23 @@ public class Cuboid {
         width = 1.0;
         height = 1.0;
     }
+
+    // UNDERSTAND: Creates a cuboid with given dimensions
+    Cuboid(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    // UNDERSTAND: Surface Area = 2(lw + lh + wh)
+    double calculateSurfaceArea() {
+        return 2 * (length * width +
+                length * height +
+                width * height);
+    }
+
+    // UNDERSTAND: Volume = l × w × h
+    double calculateVolume() {
+        return length * width * height;
+    }
 }

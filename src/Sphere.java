c4b5f29f31e012fc8b14be93a4a4aadca60df157
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 20:55:39 +0800

    Added private double radius and constructor

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,2 +1,15 @@
 public class Sphere {
+
+    // UNDERSTAND: Stores the radius of the sphere
+    private double radius;
+
+    // UNDERSTAND: Default constructor
+    Sphere() {
+        radius = 1.0;
+    }
+
+    // UNDERSTAND: Creates a sphere with a given radius
+    Sphere(double radius) {
+        this.radius = radius;
+    }
 }

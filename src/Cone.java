237847bbfab59a72b94e1e795bb087f76c60e2d4
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 21:03:10 +0800

    Added private double radius and height and constructor

--- src/Cone.java
+++ src/Cone.java
@@ -1,2 +1,12 @@
 public class Cone {
+
+    // UNDERSTAND: Stores the radius and height
+    private double radius;
+    private double height;
+
+    // UNDERSTAND: Default values
+    Cone() {
+        radius = 1.0;
+        height = 1.0;
+    }
 }

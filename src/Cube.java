735ceb26b1c0d69847e09a628451ff2c375309a5
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-04 20:40:01 +0800

    Added for private double side and default constructor

--- src/Cube.java
+++ src/Cube.java
@@ -1,2 +1,16 @@
 public class Cube {
+
+    // UNDERSTAND: Stores the side length of the cube
+    private double side;
+
+    // UNDERSTAND: Default constructor
+    Cube() {
+        side = 1.0;
+    }
+
+    // UNDERSTAND: Creates a cube with a given side length
+    Cube(double side) {
+        this.side = side;
+    }
+
 }

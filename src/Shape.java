5c21e7ec19b1e9564675f4c1504b5dfcc297ede3
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-13 21:18:47 +0800

    Added Measurable 3D.java Shape.java and

---
+++ src/Shape.java
@@ -0,0 +1,24 @@
+public abstract class Shape {
+    protected String color;
+
+    public Shape() {
+        this.color = "red";
+    }
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape[color=" + color + "]";
+    }
+}

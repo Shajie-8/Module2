70dcc1e098f5c3e82ca536e6f1ad9486fff5d4f6
Shajie-8 <shajie.macabalang@gmail.com>
2026-06-14 12:02:28 +0800

    Added setRadius

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -18,6 +18,7 @@ public class Cylinder extends Shape3D {
         this.height = height;
     }
 
+    // Getters
     public double getRadius() {
         return radius;
     }
@@ -25,6 +26,17 @@ public class Cylinder extends Shape3D {
     public double getHeight() {
         return height;
     }
+
+    // Setters
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            IO.println("Error: Radius must be positive.");
+            IO.println("Radius remains: " + this.radius);
+            return;
+        }
+        this.radius = radius;
+    }
+
 
     // UNDERSTAND: Surface Area = 2πr(r + h)
     double calculateSurfaceArea() {
